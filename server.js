b3088c4205fcd6620ee133f3b3d4158a07ad2b50
carlos gonzalez <c.gonzalez664@alumnos.santotomas.cl>
2026-05-29 20:37:18 -0400

    mesalabCG-JT

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Servidor Node OK\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = server;
